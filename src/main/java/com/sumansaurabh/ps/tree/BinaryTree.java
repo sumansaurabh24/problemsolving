de023add9471495bc9a7b199094967fe0d7aa863
Suman Saurabh <saurabh.kaystha@gmail.com>
2019-09-26 14:52:14 +0530

    Tree and Linked list problem

--- src/main/java/com/sumansaurabh/ps/tree/BinaryTree.java
+++ src/main/java/com/sumansaurabh/ps/tree/BinaryTree.java
@@ -15,6 +15,29 @@ public class BinaryTree {
         return 1+ Math.max(height(node.left) , height(node.right));
     }
 
+    Node findLCA(int n1, int n2){
+        return findLCA(root, n1, n2);
+    }
+
+    Node findLCA(Node node , int n1, int n2){
+        if(node == null){
+            return null;
+        }
+
+        if(node.data == n1 || node.data == n2){
+            return node;
+        }
+
+        Node leftLca = findLCA(node.left, n1, n2);
+        Node rightLca = findLCA(node.right, n1, n2);
+
+        if(leftLca != null && rightLca != null){
+            return node;
+        }
+
+        return (leftLca != null) ? leftLca : rightLca;
+    }
+
     public static void main(String[] args) {
 
         BinaryTree tree = new BinaryTree();
